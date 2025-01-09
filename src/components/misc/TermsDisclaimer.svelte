8f58778d82c3ed24decad5145b1c0bcafdfdb600
osoclos <osoclosus@gmail.com>
2025-01-09 10:16:07 +0800

    fixed sizing and gap of terms disclaimer

--- src/components/misc/TermsDisclaimer.svelte
+++ src/components/misc/TermsDisclaimer.svelte
@@ -39,14 +39,14 @@
 </script>
 
 <div bind:this={container} class="grid fixed top-0 left-0 z-10 place-items-center p-0 sm:p-8 w-full h-full bg-[#00000060] {termsAcknowledged ? "opacity-0" : "opacity-100"} transition-opacity duration-600" ontransitionend={({ target }) => target === container && container.classList.replace("grid", "hidden")}>
-    <div class="flex flex-col gap-8 sm:gap-4 items-center pt-6 pb-4 px-4 mx-4 max-w-160 bg-secondary rounded-lg">
+    <div class="flex flex-col gap-4 sm:gap-3 items-center pt-6 pb-4 px-4 mx-4 max-w-160 bg-secondary rounded-lg">
         <div class="flex flex-col gap-2">
             <Header title="DISCLAIMER" />
             <p class="font-subtitle text-active text-center">{localStorage.getItem("terms-acknowledged") ? `CultiVis has updated its terms of service ${isTOSAmended() ? "again today" : `on ${getDate()}`}. You may view the new terms below or close this popup.` : "CultiVis requires you to agree and acknowledge the CultiVis Terms of Service. You may view the terms below or close this popup."}</p>
         </div>
 
-        <div class="flex flex-col gap-2 w-80">
-            <Notice class="text-sm not-sm:pb-2" label="Closing this popup will mean you agree with the Terms of Service." />
+        <div class="flex flex-col gap-2 w-80 sm:w-fit">
+            <Notice class="not-sm:pb-2 not-sm:text-sm" label="Closing this popup will mean you agree with the Terms of Service." />
             <List class="flex flex-col sm:flex-row justify-center items-center" enableKeyInput={!termsAcknowledged} isHorizontal={!isOnPhone} focusFirst>
                 <BannerButton label="View Terms" href="https://github.com/osoclos/cultivis/blob/main/ToS.md" />
                 <BannerButton label="Close and Accept" {onclick} />
