75a49d3c14472b24bd97ecea8e2fc0313fb30945
osoclos <osoclosus@gmail.com>
2025-01-16 20:19:23 +0800

    separated lib/spine-ts from public folder and deferred spine script for better performance and load times

--- setup/generate.ts
+++ setup/generate.ts
@@ -21,7 +21,7 @@ const MAX_TILE_HEIGHT: number = 64;
 const app = express();
 const data = multer();
 
-app.get("/spine-webgl.min.js", (_, res) => res.sendFile(path.join(__dirname, "../public/lib/spine-ts/build/spine-webgl.min.js")));
+app.get("/spine-webgl.js", (_, res) => res.sendFile(path.join(__dirname, "../lib/spine-ts/build/spine-webgl.js")));
 app.use("/assets", express.static(path.join(__dirname, "../public/assets")));
 
 app.post("/followers", data.array("files"), (req) => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,56 @@
-import { defineConfig } from "vite";
+import path from "path";
+import fs from "fs/promises";
+
+import { defineConfig, Plugin } from "vite";
+import { minify } from "terser";
 
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import mkcert from "vite-plugin-mkcert";
 
+const spineBundlePlugin = (): Plugin => ({
+    name: "spine-bundler",
+    apply: "build",
+
+    async generateBundle() {
+        const input = await fs.readFile(path.join(__dirname, "lib/spine-ts/build/spine-webgl.js"), "utf-8");
+        const { code: output = "" } = await minify(input);
+
+        const filepath = path.join(__dirname, "dist/scripts/lib/spine-webgl.min.js");
+
+        await fs.mkdir(path.dirname(filepath), { recursive: true });
+        await fs.writeFile(filepath, output, "utf-8");
+    },
+
+    transformIndexHtml(html: string): string {
+        html = html.replace("\n\n        <script src=\"lib/spine-ts/build/spine-webgl.js\"></script>", "");
+        html = html.replace("</title>", "</title>\n      <script src=\"scripts/lib/spine-webgl.min.js\" defer></script>");
+
+        return html;
+    }
+});
 
 // https://vite.dev/config/
 export default defineConfig({
-    build: { sourcemap: true },
-    server: { fs: { deny: ["setup/**/*.ts"] } },
+    server: { fs: { allow: ["index.html", "src", "public", "lib"] } },
+    build: {
+        assetsDir: ".",
+        sourcemap: true,
 
-    plugins: [svelte({ compilerOptions: { runes: true } }), tailwindcss(), mkcert()]
+        rollupOptions: {
+            output: {
+                entryFileNames: "scripts/[name]-[hash].js",
+                chunkFileNames: "scripts/[name]-[hash].js",
+                assetFileNames: ({ names }) => names[0].endsWith(".css") ? "styles/[name]-[hash][extname]" : "assets/[name]-[hash][extname]",
+
+                manualChunks(id) {
+                    if (id.includes("node_modules")) return "vendor";
+                }
+            },
+
+            external: (id) => id.startsWith("/src/lib/")
+        }
+    },
+
+    plugins: [svelte({ compilerOptions: { runes: true } }), tailwindcss(), mkcert(), spineBundlePlugin()]
 });
